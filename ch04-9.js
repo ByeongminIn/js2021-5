44ca53d4a722bf71f0cf72abab52d89e9b7635ce
byeongminin <dlsqudals@gmail.com>
2021-04-09 08:59:55 +0900

    4월6일수업

--- ch04-9.js
+++ ch04-9.js
@@ -52,4 +52,18 @@ console.log(foo);
 // shift는 배열의 첫번째 주소에 있는 값을 제거한 후에 반환해주는 함수
 console.log("\n shift");
 foo.shift();
+bar.shift();
 console.log(foo);
+console.log(bar);
+
+// push와 pop을 이용하면 stack으로 이용할 수 있다.
+// push와 shift를 이용하면 queue로 이용할 수 있다.
+
+//concat은 두개의 배열을 합쳐주는 함수.
+console.log("\n concat");
+
+// reverse은 배열을 역순으로 재배치.
+console.log("\n reverse");
+
+// sort는 배열을 정렬;
+console.log("\n sort");
