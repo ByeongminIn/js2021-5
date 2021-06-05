6d2d5806be260fac7ec54b3740d381c6e9750b5c
ByeongminIn <dlsqudals@gmail.com>
2021-06-05 18:43:02 +0900

    6월 1일 수업내용

--- ch10.js
+++ ch10.js
@@ -1,6 +1,6 @@
 // 예제 10-1
 
-const express = require('express');
+/* const express = require('express');
 
 const app = express();
 
@@ -10,4 +10,19 @@ app.use((request,response) => {
 
 app.listen(52273, () => {
     console.log('Server running at http://127.0.0.1:52237');
+}); */
+
+// 예제 10-2
+const express = require('express');
+
+const app = express();
+
+app.get('/page/:id',(request,response) => {
+    const id = request.params.id;
+
+    response.send(`<h1>${id} Page</h1>`);
+});
+
+app.listen(52273, () => {
+    console.log('Server running at http://127.0.0.1:52273');
 });
